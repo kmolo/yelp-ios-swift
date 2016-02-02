549148a64b75bef0a64bba90a4407897a183bea2
Katherine Moloney <kmoloney@mail.ccsf.edu>
2016-02-01 23:05:50 -0800

    implement 2 required functions for protocol UITableViewDataSource
    - number of RowsInSection
    - cellForRowAtIndexPath

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -40,7 +40,19 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         }
 */
     }
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses != nil {
+            return businesses!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BusinessCell", forIndexPath: indexPath) as! BusinessCell
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
